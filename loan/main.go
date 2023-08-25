010560064f24d7a53bec1a17b61a770f676186d3
Kostis Kapelonis <kostis@codefresh.io>
2023-08-25 15:03:52 +0300

    Handle communication with backend

--- loan/main.go
+++ loan/main.go
@@ -19,8 +19,8 @@ type LoanApplication struct {
 	BackendVersion string
 	BackendHost    string
 	BackendPort    string
-	InterestRate   int
 	LoanAmount     int
+	LoanResult     string
 }
 
 func main() {
@@ -62,31 +62,6 @@ func main() {
 		fmt.Fprintln(w, "yes")
 	})
 
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-	// 	loanAmount := parseLoanAmount(r)
-
-	// 	quote := ""
-	// 	interestFound, err := getInterestRate(loanApp)
-	// 	if err != nil {
-	// 		log.Println("Interest error :", err)
-	// 		quote = "Could not get interest. Sorry!"
-	// 	} else {
-	// 		quote = offerQuote(loanAmount, interestFound)
-	// 	}
-
-	// 	fmt.Fprintf(w, `<html>
-	// 	<form method="post">
-	// 	Enter your loan amount to see the interest. $<input name="loan" type="number" value="%d">
-	// 	<br/>
-	// 	<input type="submit">
-	// 	</form>
-	// 	<br/>
-	// 	%s
-	// 	</html>
-	// 	`, loanAmount, quote)
-	// })
-
 	http.HandleFunc("/", loanApp.serveFiles)
 
 	fmt.Printf("Frontend version %s is listening now at port %s\n", loanApp.AppVersion, port)
@@ -94,7 +69,7 @@ func main() {
 	log.Fatal(err)
 }
 
-func (loanApp LoanApplication) serveFiles(w http.ResponseWriter, r *http.Request) {
+func (loanApp *LoanApplication) serveFiles(w http.ResponseWriter, r *http.Request) {
 	upath := r.URL.Path
 	p := "." + upath
 	if p == "./" {
@@ -106,7 +81,10 @@ func (loanApp LoanApplication) serveFiles(w http.ResponseWriter, r *http.Request
 	http.ServeFile(w, r, p)
 }
 
-func (loanApp LoanApplication) home(w http.ResponseWriter, r *http.Request) {
+func (loanApp *LoanApplication) home(w http.ResponseWriter, r *http.Request) {
+
+	loanApp.handleFormSubmission(w, r)
+
 	t, err := template.ParseFiles("./static/index.html")
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -119,6 +97,25 @@ func (loanApp LoanApplication) home(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Error executing template: %v", err)
 		return
 	}
+}
+
+func (loanApp *LoanApplication) handleFormSubmission(w http.ResponseWriter, r *http.Request) {
+	loanAmount := parseLoanAmount(r)
+	loanApp.LoanAmount = loanAmount
+	if loanAmount == 0 {
+		return
+	}
+
+	quote := ""
+	interestFound, err := loanApp.getInterestRate()
+	if err != nil {
+		log.Println("Interest error :", err)
+		quote = "Could not get interest. Sorry!"
+	} else {
+		quote = offerQuote(loanAmount, interestFound)
+	}
+	loanApp.LoanResult = quote
+
 }
 
 func parseLoanAmount(r *http.Request) int {
@@ -148,12 +145,12 @@ func offerQuote(loan int, interest int) string {
 
 }
 
-func getInterestRate(loanApplication LoanApplication) (rate int, err error) {
+func (loanApp *LoanApplication) getInterestRate() (rate int, err error) {
 	url, err := url.Parse("http://interest:8080/api/v1/interest")
 	if err != nil {
 		log.Fatal(err)
 	}
-	url.Host = loanApplication.BackendHost + ":" + loanApplication.BackendPort
+	url.Host = loanApp.BackendHost + ":" + loanApp.BackendPort
 
 	resp, err := http.Get(url.String())
 	if err != nil {
